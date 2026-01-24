9831ffb256f6c3eadc63e937da8b4f9dc39aacba
Dejan Radovanovic <dradovanovic@tcpsoftware.com>
2026-01-24 22:08:44 +0100

    Initialize sailing nations table

--- src/main/java/com/fleetscore/sailingnation/domain/SailingNation.java
+++ src/main/java/com/fleetscore/sailingnation/domain/SailingNation.java
@@ -2,8 +2,6 @@ package com.fleetscore.sailingnation.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -21,7 +19,6 @@ import lombok.Setter;
 public class SailingNation {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 3)
